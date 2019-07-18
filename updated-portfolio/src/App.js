1ae80666bb7a67fb0ee60431d5f05debdca3745a
Affiliatethis <chris@onecrookedtree.com>
2019-07-18 16:30:22 -0400

    First Push of React MDL.

---
+++ updated-portfolio/src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+// import { Nav, Navbar, NavItem, NavDropdown } from "react-bootstrap";
+import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
+// import * as ReactMDL from 'react-mdl';
+
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div style={{ height: "300px", position: "relative" }}>
+        <Layout
+          style={{
+            background:
+              "url(http://www.getmdl.io/assets/demos/transparent.jpg) center / cover"
+          }}
+        >
+          <Header transparent title="Title" style={{ color: "white" }}>
+            <Navigation>
+              <a href="/">Link</a>
+              <a href="/">Link</a>
+              <a href="/">Link</a>
+              <a href="/">Link</a>
+            </Navigation>
+          </Header>
+          <Drawer title="Title">
+            <Navigation>
+              <a href="/">Link</a>
+              <a href="/">Link</a>
+              <a href="/">Link</a>
+              <a href="/">Link</a>
+            </Navigation>
+          </Drawer>
+          <Content />
+        </Layout>
+      </div>
+    );
+  }
+}
+
+export default App;
