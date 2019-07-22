b8d903e3222aab11833b8dc9552571752f18dd42
Affiliatethis <chris@onecrookedtree.com>
2019-07-22 01:34:26 -0400

    beginning to work on the projects page along with cards per set of language projects.

--- updated-portfolio/src/Components/projects.js
+++ updated-portfolio/src/Components/projects.js
@@ -1,12 +1,63 @@
 import React, { Component } from "react";
+import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardActions, Button, CardMenu, IconButton } from "react-mdl";
 
 class Projects extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = { activeTab: 0 };
+  }
+  toggleCategories() {
+  if(this.state.activeTab === 0) {
+    return(
+      <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+        <CardTitle style={{color: '#fff', height: '176px', bakcground: 'url()'}} ></CardTitle>
+
+      </Card>
+    )
+
+  } else if(this.state.activeTab === 1) {
     return (
-      <div>
-        <h1>Projects </h1>
+      <div><h1>This is Mongodb</h1></div>
+    )
+
+  } else if(this.state.activeTab === 2) {
+    return (
+      <div><h1>This is Express</h1></div>
+    )
+ 
+} else if(this.state.activeTab === 3) {
+  return (
+    <div><h1>This is JavaScript</h1></div>
+  )
+  };
+
+}
+  render()  {
+    return (
+      <div className="category-tabs">
+        <Tabs
+          activeTab={this.state.activeTab}
+          onChange={(tabId) => this.setState({ activeTab: tabId })}
+          ripple
+        >
+          <Tab>React</Tab>
+          <Tab>Mongodb</Tab>
+          <Tab>Express</Tab>
+          <Tab>Javascript</Tab>
+        </Tabs>
+
+        <section className="projects-grid">
+          <Grid className="projects-grid">
+            <Cell col={12}>
+              <div className="content">{this.toggleCategories()}</div>
+            </Cell>
+
+          </Grid>
+          
+        </section>
+
       </div>
-    );
+    )
   }
 }
 
